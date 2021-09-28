9adbde0e808a10fe9f2160cffd63384ad9f8066a
Tim Maas <tim.maas@conclusion.nl>
2021-09-28 11:28:55 +0200

    added AwesomeAnimals Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 
 // import LikeCounter from "./components/LikeCounter";
-import LikeButton from "./components/LikeButton";
+// import LikeButton from "./components/LikeButton";
+import AwesomeAnimals from "./components/AwesomeAnimals";
 
 function App() {
   return (
     <main>
       <h1>Some Simple Title</h1>
-      <LikeButton />
+      <AwesomeAnimals />
     </main>
   );
 }
